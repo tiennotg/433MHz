83fc4c85c7875a65132fbf382c726740c7db6836
Guilhem Tiennot <guilhem@tiennotblog.fr>
2021-03-11 23:49:14 +0100

    Update 433Manchester.ino

--- 433Manchester.ino
+++ 433Manchester.ino
@@ -19,7 +19,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with Sirène.py.  If not, see <https://www.gnu.org/licenses/>.
+ * along with 433Manchester.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */ 
 
